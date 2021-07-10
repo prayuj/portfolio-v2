a56a2fe291ffe4dee6ffd24b37c22fdab0932d68
Prayuj Pillai <prayuj.pillai@pens.com>
2021-07-11 01:42:32 +0530

    added icons

--- src/components/about.js
+++ src/components/about.js
@@ -1,4 +1,6 @@
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Badge } from 'react-bootstrap';
+import { FaReact, FaHtml5, FaCss3Alt, FaNodeJs, FaPython } from 'react-icons/fa';
+import { SiJavascript, SiMongodb, SiJquery} from 'react-icons/si'
 import {StyledLink} from '../styled-components/styles'
 import styled from 'styled-components';
 
@@ -19,12 +21,34 @@ const AccentedIndex = styled.span`
     margin-right: 10px;
 `
 
+const StyledBadge = styled(Badge)`
+    font-size:90%;
+    margin: 0.3em;
+`
+
 const About = ({id}) => {
     const one = <StyledSpan><h5><AccentedIndex>[1]</AccentedIndex></h5><h3>About me</h3></StyledSpan>
     const two = <p>Hello! My name is Prayuj and I am a Software Engineer and I enjoy Full Stack Development.</p>
     const three = <p>I currently work at <StyledLink target="_blank" rel="noreferrer" href="http://urls.prayuj.tech/t/cimpress" className="content-hover-link">Cimpress</StyledLink> as part of their Business subsidiary, <StyledLink target="_blank" rel="noreferrer" href="http://urls.prayuj.tech/t/np" className="content-hover-link">National Pen.</StyledLink></p>
     const four = <p>Here are a few technologies I am proficient in:</p>
-    const items = [one, two, three, four]
+
+    const technology = [
+        { icon: SiJavascript, name: 'Javascript', color: '#EFD81D'},
+        { icon: FaReact, name: 'React', color: '#09D8F2' },
+        { icon: FaHtml5, name: 'HTML', color: '#D84924'},
+        { icon: FaCss3Alt, name: 'CSS', color: '#2449D8'},
+        { icon: FaNodeJs, name: 'NodeJs', color: '#6AB24D'},
+        { icon: SiMongodb, name: 'Mongodb', color: '#108B4B'},
+        { icon: SiJquery, name: 'jQuery', color: '#0764A4' },
+        { icon: FaPython, name: 'Python', color: '#F2C53D'}
+    ]
+
+    const five = <p style={{textAlign: 'center'}}>
+        {technology.map(tech => <StyledBadge pill style={{ background: tech.color }}>
+            <tech.icon /> {tech.name}
+        </StyledBadge>)}
+        </p>
+    const items = [one, two, three, four, five]
     return (
         <StyledContainer id={id} fluid="lg">
             <Row>
