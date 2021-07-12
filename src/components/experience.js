94b82ff1bc2d341363b0ac4aae4166f29b6a9510
Prayuj Pillai <prayuj.pillai@pens.com>
2021-07-12 17:28:46 +0530

    updated experience

--- src/components/experience.js
+++ src/components/experience.js
@@ -3,10 +3,21 @@ import styled from 'styled-components';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import { StyledLink, StyledSpan, AccentedIndex, StyledContainer, StyledRow, StyledCol } from '../utils/styles'
 import { experiences } from '../utils/const'
+import Card from 'react-bootstrap/Card';
+
+const StyledCard = styled(Card)`
+    background: var(--dark-color);
+    border: 1px solid var(--accent-color);
+    height: 100%;
+`
 
 const StyledJumbotron = styled(Jumbotron)`
     background: var(--dark-color);
 `
+
+const cardColumnStyles = {
+    paddingBottom: '15px'
+}
 
 const Experience = ({id}) => {
 
@@ -15,23 +26,28 @@ const Experience = ({id}) => {
             <StyledRow>
                 <StyledCol>
                     <StyledSpan><h5><AccentedIndex>[2]</AccentedIndex></h5><h3>My Experience</h3></StyledSpan>
-                    {experiences.map((experience)=>
-                        <StyledJumbotron fluid>
-                            <Container>
-                                <h4><StyledLink target="_blank" rel="noreferrer" href={experience.link}>{experience.organization}</StyledLink></h4>
-                                <h5>{experience.designation} </h5>
-                                <h6>{experience.duration}</h6>
-                                <ul>
-                                    {experience.description.map((point, index) => (
-                                        <li className="display-flex">
-                                            <p>{point}</p>
-                                        </li>
-                                    ))}
-                                </ul>
-                            </Container>
-                        </StyledJumbotron>
-                    )}
                 </StyledCol>
+            </StyledRow>
+            <StyledRow xs={1} md={1} lg={1} xl={1}>
+                {experiences.map((experience) =>
+                    <StyledCol style={cardColumnStyles}>
+                        <StyledCard>
+                            <Card.Body>
+                                <Card.Title>{experience.designation} @ <StyledLink target="_blank" rel="noreferrer" href={experience.link}>{experience.organization}</StyledLink></Card.Title>
+                                <Card.Subtitle className="mb-2 text-muted">{experience.duration}</Card.Subtitle>
+                                <Card.Text>
+                                    <ul>
+                                        {experience.description.map((point, index) => (
+                                            <li className="display-flex">
+                                                <p>{point}</p>
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </Card.Text>
+                            </Card.Body>
+                        </StyledCard>
+                    </StyledCol>)
+                }
             </StyledRow>
         </StyledContainer>
     );
